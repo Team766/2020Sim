d792acabbb8d4bdc04d2fa3a4f210fefc1296f89
Mason Lau <masonrlau@gmail.com>
2017-01-14 17:24:45 -0800

    Brett's Changes Roll-up:
    Balls exit hopper with an initial velocity
    
    Counts gears, relocates orgion of hoppers to center, and spawns in
    balls at hoppers

---
+++ Assets/ReleaseBalls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleaseBalls : MonoBehaviour {
+    public GameObject hopper1;
+    public GameObject hopper2;
+    public GameObject ball;
+
+    public Vector3 outputVel;
+
+    public bool empty;
+    public int ballsPerHopper = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (empty || other == null)
+            return;
+        if(other.transform.parent.tag == "Player")
+        {
+            for (int i = 0; i < ballsPerHopper; i++)
+            {
+                GameObject.Instantiate(ball, hopper1.transform.position, new Quaternion()).GetComponent<Rigidbody>().velocity = outputVel;
+                GameObject.Instantiate(ball, hopper2.transform.position, new Quaternion()).GetComponent<Rigidbody>().velocity = outputVel;
+            }
+            empty = true;
+        }
+    }
+}
