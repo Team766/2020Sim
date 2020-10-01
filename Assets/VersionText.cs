c6cc5aba9f469bada74a3c662892a82a3f7f6870
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-10-01 09:21:00 -0700

    Add version text

---
+++ Assets/VersionText.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class VersionText : MonoBehaviour {
+
+    void Start() {
+        GetComponent<Text>().text = "Version " + Application.version;
+    }
+
+}
