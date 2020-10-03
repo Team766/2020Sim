89379550322d9e7669c181eb61b2f50fe277122e
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-10-03 10:47:33 -0700

    Use art-only objects for held balls
    Fixes #11

--- Assets/HoldObject.cs
+++ Assets/HoldObject.cs
@@ -1,30 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class HoldObject : MonoBehaviour {
-
-    public Rigidbody holding;
-    
-    public bool Grab(Rigidbody obj) {
-        if (holding)
-            return false;
-        
-        holding = obj;
-        holding.transform.position = this.transform.position;
-        holding.transform.rotation = this.transform.rotation;
-        holding.transform.parent = this.transform;
-        holding.isKinematic = true;
-        
-        return true;
-    }
-
-    public Rigidbody Drop() {
-        var obj = holding;
-        holding = null;
-        if (obj) {
-            obj.isKinematic = false;
-            obj.transform.parent = null;
-        }
-        return obj;
+    public void SetState(bool holding) {
+        GetComponent<MeshRenderer>().enabled = holding;
     }
 }
