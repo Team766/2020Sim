199ba3b88d995986de54b27f85aab1529bd55e00
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-11-30 01:41:21 -0800

    Add Hammer V1 robot

---
+++ Assets/BattleBots/HammerV1/AlignToObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignToObject : MonoBehaviour {
+    public Transform other;
+
+    void Update() {
+        var rotation = new Quaternion();
+        rotation.SetLookRotation(other.position - this.transform.position);
+        this.transform.rotation = rotation;
+    }
+}
